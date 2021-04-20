be4151f8ea6797a4536274eae55cc7b715efcd01
agusp-dev <perezp.agustin@gmail.com>
2021-04-20 03:00:43 -0300

    Integrated row image

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className='container'>
       <div className='row justify-content-center mt-4'>
-        <div className='col-8'>
+        <div className='col-12'>
           <UsersTable />
         </div>
       </div>
--- src/components/table/UsersTable.jsx
+++ src/components/table/UsersTable.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react'
 import TableHead from './TableHead'
 import TableBody from './TableBody'
+import ProfilePhoto from '../../assets/img-profile.png'
 
 const data = {
   head: [
     '#',
+    'Image',
     'Name',
     'Surname',
     'Gender',
@@ -12,22 +14,25 @@ const data = {
   ],
   body: [
     {
-      'name': 'Agustin',
-      'surname': 'Perez',
-      'gender': 'Male',
-      'email': 'perez.agustin@gmail.com'
+      image: ProfilePhoto,
+      name: 'Agustin',
+      surname: 'Perez',
+      gender: 'Male',
+      email: 'perez.agustin@gmail.com'
     },
     {
-      'name': 'Diego',
-      'surname': 'Perez',
-      'gender': 'Male',
-      'email': 'perez.diego@gmail.com'
+      image: ProfilePhoto,
+      name: 'Diego',
+      surname: 'Perez',
+      gender: 'Male',
+      email: 'perez.diego@gmail.com'
     },
     {
-      'name': 'Maximiliano',
-      'surname': 'Perez',
-      'gender': 'Male',
-      'email': 'perez.maxi@gmail.com'
+      image: ProfilePhoto,
+      name: 'Maximiliano',
+      surname: 'Perez',
+      gender: 'Male',
+      email: 'perez.maxi@gmail.com'
     }
   ]
 }
